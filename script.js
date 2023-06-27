8d74be61d11628085df5f9f3a724a5c4838ad8eb
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-27 12:46:02 -0700

    this is the answer of zero-and-one

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Good Luck 💪🏾
+function ZeroAndOne(arr) {
+    let result = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      var element = arr[i];
+  
+      if (element === 'Zero') {
+        result.push(0);
+      } else if (element === 'One') {
+        result.push(1);
+      }
+    }
+  
+    return result;
+  }
+  
+  var array1 = ['one', 'Zero', 'Zero', 'One'];
+  var array2 = ['Zero', 'ONE', 'one', 'Zero'];
+  
+  console.log(ZeroAndOne(array1));
+  console.log(ZeroAndOne(array2)); 
